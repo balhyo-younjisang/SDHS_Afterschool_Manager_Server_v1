3056d62baf5a0f9893f8c7e322eba3e7c76fce38
unknown <yjs12180825@gmail.com>
2024-03-05 19:46:58 +0900

    :technologist: Entity Column 이름에 언더스코어 추가 :technologist:

--- src/main/java/com/yunjisang/afterschool/afterschool/domain/Lecture/domain/Lecture.java
+++ src/main/java/com/yunjisang/afterschool/afterschool/domain/Lecture/domain/Lecture.java
@@ -35,10 +35,10 @@ public class Lecture {
     private LectureDivision division; // 분류
 
     @Column(nullable = false)
-    private String lectureroom; // 수업장소
+    private String lecture_room; // 수업장소
 
     @Column(nullable = false)
-    private Long headcount; // 인원수
+    private Long head_count; // 인원수
 
     @Column(nullable = false)
     private String target; // 대상
@@ -47,13 +47,13 @@ public class Lecture {
     private Method method; // 운영형태
 
     @Column(nullable = false)
-    private String selectmethod; // 선별 방법
+    private String select_method; // 선별 방법
 
     @Column(nullable = false)
-    private LocalDateTime startdate; // 방과후 시작 날짜
+    private LocalDateTime start_date; // 방과후 시작 날짜
 
     @Column(nullable = false)
-    private LocalDateTime enddate; // 방과후 종료 날짜
+    private LocalDateTime end_date; // 방과후 종료 날짜
 
     @Column(nullable = false)
     private Day day; // 진행 요일
@@ -62,7 +62,7 @@ public class Lecture {
     private Long duration; // 일당 수업시간
 
     @Column(nullable = false)
-    private Long allhours; // 총 수업 시수
+    private Long all_hours; // 총 수업 시수
 
     @Column(nullable = false)
     private Long tuition; // 예상 수강료
