e94ebbbacccb2656050a2d1030f2a87723e3503c
unknown <yjs12180825@gmail.com>
2024-03-06 13:05:11 +0900

    :sparkles: Lecture Service & Repository 작성 :sparkles:

--- src/main/java/com/yunjisang/afterschool/afterschool/domain/Lecture/repository/LectureRepository.java
+++ src/main/java/com/yunjisang/afterschool/afterschool/domain/Lecture/repository/LectureRepository.java
@@ -1,16 +1,22 @@
 package com.yunjisang.afterschool.afterschool.domain.Lecture.repository;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.yunjisang.afterschool.afterschool.domain.Lecture.domain.Lecture;
+import com.yunjisang.afterschool.afterschool.domain.Lecture.domain.LectureDivision;
 
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
     Optional<Lecture> findByName(String name);
 
-    Optional<List<Lecture>> findAllByTeacher(String teacher);
+    Optional<Page<Lecture>> findAllByTeacher(String teacher, Pageable pa);
+
+    Page<Lecture> findAllByNameContaining(String keyword, Pageable pageable);
+
+    Page<Lecture> findAllByDivision(LectureDivision lectureDivision, Pageable pageable);
 }
--- src/main/java/com/yunjisang/afterschool/afterschool/domain/Lecture/service/LectureService.java
+++ src/main/java/com/yunjisang/afterschool/afterschool/domain/Lecture/service/LectureService.java
@@ -1,11 +1,13 @@
 package com.yunjisang.afterschool.afterschool.domain.Lecture.service;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.yunjisang.afterschool.afterschool.domain.Lecture.domain.Lecture;
+import com.yunjisang.afterschool.afterschool.domain.Lecture.domain.LectureDivision;
 import com.yunjisang.afterschool.afterschool.domain.Lecture.repository.LectureRepository;
 import com.yunjisang.afterschool.afterschool.global.exception.CustomException;
 import com.yunjisang.afterschool.afterschool.global.exception.ErrorCode;
@@ -17,12 +19,23 @@ import lombok.RequiredArgsConstructor;
 public class LectureService {
     private final LectureRepository lectureRepository;
 
+    /**
+     * Lecture Entity 를 리턴
+     * 
+     * @param lectureId
+     * @return Lecture
+     */
     public Lecture getLectureInfo(Long lectureId) {
         Lecture lecture = lectureRepository.findById(lectureId)
                 .orElseThrow(() -> new CustomException(ErrorCode.LECTURE_NOT_FOUND));
         return lecture;
     }
 
+    /**
+     * 인자로 들어온 Entity를 Database에 저장
+     * 
+     * @param lecture
+     */
     public void save(Lecture lecture) {
         Optional<Lecture> alreadyLecture = lectureRepository.findByName(lecture.getName());
 
@@ -32,9 +45,38 @@ public class LectureService {
         lectureRepository.save(lecture);
     }
 
-    public List<Lecture> findLectureByTeacher(String teacher) {
-        List<Lecture> lectures = lectureRepository.findAllByTeacher(teacher)
+    /**
+     * 인자로 받은 teacher 값을 Database에서 찾아 Paging을 통해 리턴
+     * 
+     * @param teacher
+     * @param pageable
+     * @return Page<Lecture>
+     */
+    public Page<Lecture> findLecturesByTeacher(String teacher, Pageable pageable) {
+        Page<Lecture> lectures = lectureRepository.findAllByTeacher(teacher, pageable)
                 .orElseThrow(() -> new CustomException(ErrorCode.TEACHER_NOT_FOUND));
         return lectures;
+    }
+
+    /**
+     * 인자로 받은 검색어를 통해 Database에서 검색 후 Paging 처리를 해 반환
+     * 
+     * @param keywords
+     * @param pageable
+     * @return Page<Lecture>
+     */
+    public Page<Lecture> searchLecture(String keywords, Pageable pageable) {
+        return lectureRepository.findAllByNameContaining(keywords, pageable);
+    }
+
+    /**
+     * 강의 분류를 통해 강의를 찾아 반환
+     * 
+     * @param lectureDivision
+     * @param pageable
+     * @return
+     */
+    public Page<Lecture> findLecturesByDivision(LectureDivision lectureDivision, Pageable pageable) {
+        return lectureRepository.findAllByDivision(lectureDivision, pageable);
     }
 }
