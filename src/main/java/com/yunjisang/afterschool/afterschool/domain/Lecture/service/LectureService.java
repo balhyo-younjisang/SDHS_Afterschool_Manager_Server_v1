df011d80e56ed22086892bb5e7a9e3c6aa53699e
unknown <yjs12180825@gmail.com>
2024-03-06 11:29:46 +0900

    :memo: getLectureInfo Exception 핸들링 :memo:

--- src/main/java/com/yunjisang/afterschool/afterschool/domain/Lecture/service/LectureService.java
+++ src/main/java/com/yunjisang/afterschool/afterschool/domain/Lecture/service/LectureService.java
@@ -1,10 +1,11 @@
 package com.yunjisang.afterschool.afterschool.domain.Lecture.service;
 
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.yunjisang.afterschool.afterschool.domain.Lecture.domain.Lecture;
 import com.yunjisang.afterschool.afterschool.domain.Lecture.repository.LectureRepository;
+import com.yunjisang.afterschool.afterschool.global.exception.CustomException;
+import com.yunjisang.afterschool.afterschool.global.exception.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,7 +15,8 @@ public class LectureService {
     private final LectureRepository lectureRepository;
 
     public Lecture getLectureInfo(Long lectureId) {
-        Lecture lecture = lectureRepository.findById(lectureId).orElseThrow();
+        Lecture lecture = lectureRepository.findById(lectureId)
+                .orElseThrow(() -> new CustomException(ErrorCode.LECTURE_NOT_FOUND));
         return lecture;
     }
 }
